9ff4f7ddc4de3fd242130c81529814281142878f
Kurama <man.schibli@gmail.com>
2023-03-22 22:20:44 +0100

    Import Websockets into the project and first blueprint for the jwt token authentication

--- client/script.js
+++ client/script.js
@@ -1,28 +1,68 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const input = document.querySelector('.chat-input input');
-  const chat = document.querySelector('.chat-input button');
-  const messages = document.querySelector('.chat-messages');
-
-  const socket = new WebSocket('ws://localhost:8080');
-
-  socket.addEventListener('open', function(event) {
-    const messageElement = document.createElement("p");
-    messageElement.textContent = event.data;
-    messages.appendChild(messageElement);
-    chatMessages.scrollTop = chatMessages.scrollHeight;
-  });
-
-  button.addEventListener("click", sendMessage);
-  input.addEventListener("keypress", function (event) {
-    if (event.key === "Enter") {
-      sendMessage();
+function toggleNav() {
+    const sidebar = document.getElementById("sidebar");
+    const main = document.getElementById("main");
+    
+    if (sidebar.style.width === "250px") {
+        sidebar.style.width = "0";
+        main.style.marginLeft = "0";
+    } else {
+        sidebar.style.width = "250px";
+        main.style.marginLeft = "250px";
     }
-  });
-function sendMessage() {
-    const message = input.value.trim();
-    if (message.length > 0) {
-      socket.send(message);
-      input.value = "";
+}
+
+// Function to show the message
+function showmessage(isMine, text = false) {
+    const theirs = 'theirs';
+    const messageContainer = document.getElementById("message");
+    const messageRow = document.createElement("div");
+    messageRow.className = `message-row ${isMine ? 'mine' : theirs}`;
+
+    const bubble = document.createElement("div");
+    bubble.className = "bubble";
+    bubble.style.backgroundColor = isMine ? "var(--message-send)" : "var(--message-recieve)";
+    bubble.textContent = text;
+
+    messageRow.appendChild(bubble);
+    messageContainer.appendChild(messageRow);
+    messageContainer.scrollTop = messageContainer.scrollHeight;
+}
+
+const ws = new WebSocket("ws://localhost:3000");
+ws.addEventListener('message', ev => {
+    showmessage(false, ev.data);
+});
+
+document.querySelector('form').onsubmit = ev => {
+    ev.preventDefault();
+    const input = document.querySelector('input');
+    ws.send(input.value);
+    showmessage(true, input.value);
+    input.value = '';
+}
+
+async function handleSubmit(event, type) {
+    event.preventDefault();
+    const form = event.target;
+  
+    const email = form.email.value;
+    const password = form.password.value;
+    const username = type === 'register' ? form.username.value : '';
+  
+    const response = await fetch(`/api/${type}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password, username }),
+    });
+  
+    const data = await response.json();
+  
+    if (response.status === 200) {
+      localStorage.setItem('token', data.token);
+      window.location.href = 'chat/glimpse.html';
+    } else {
+      alert(data.message);
     }
   }
-});
